c8c556058ace5d6e91b494b57e655ac2dba7ef5f
owen07020 <153528559+owen07020@users.noreply.github.com>
2025-03-27 14:17:09 +0000

    Update forecast.py

--- forecast.py
+++ forecast.py
@@ -1,6 +1,9 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+CORS(app, origins=["https://cream-calculator-frontend.vercel.app"])
 
 @app.route('/api/forecast', methods=['POST'])
 def forecast():
